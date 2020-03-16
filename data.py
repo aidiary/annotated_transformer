42926ad078915206484c816ca8a920a47ef84e28
Koichiro Mori <f2forest@gmail.com>
2020-03-16 18:36:47 +0900

    Initial commit

---
+++ data.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torch
+
+
+def subsequent_mask(size):
+    # Decoderにおいて未来の出力を見ないようにするためのマスク
+    attn_shape = (1, size, size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    return torch.from_numpy(subsequent_mask) == 0
+
+
+class Batch:
+    def __init__(self, src, tgt=None, pad=0):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-2)
+        if tgt is not None:
+            self.tgt = tgt[:, :-1]
+            self.tgt_y = tgt[:, 1:]
+            self.tgt_mask = self.make_std_mask(self.tgt, pad)
+            self.ntokens = (self.tgt_y != pad).data.sum()
+
+    @staticmethod
+    def make_std_mask(tgt, pad):
+        tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = tgt_mask & subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data)
+        return tgt_mask
+
+
+def data_gen(V, batch, nbatches):
+    for i in range(nbatches):
+        data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
+        data[:, 0] = 1
+        src = data.clone().detach()
+        tgt = data.clone().detach()
+        yield Batch(src, tgt, 0)
